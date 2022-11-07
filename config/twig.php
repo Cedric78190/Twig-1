8065ceaad1057177950d0d8dc1d67e662715161f
Cedric Nkene <cedric.nkn@gmail.com>
2022-11-07 13:34:02 +0100

    quete twig

--- config/twig.php
+++ config/twig.php
@@ -1,7 +1,10 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
-$twig = new Twig\Environment($loader, ['debug' => true]);
+require_once __DIR__ . '/../vendor/twig/twig/src/Loader/FilesystemLoader.php';
+require_once __DIR__ . '/../vendor/twig/twig/src/Extension/DebugExtension.php';
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+$loader = new FilesystemLoader(__DIR__ . '/../src/View');
+$twig = new Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
